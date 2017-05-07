2c9a34d1989edc6067f4273028d2bc73927aee07
xGioe <giovannimaria.riva@studenti.unitn.it>
2017-05-07 15:17:34 +0200

    add progress bar and text label

--- iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/GpsLoadingActivity.java
+++ iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/GpsLoadingActivity.java
@@ -1,15 +1,27 @@
-package it.unitn.disi.lpsmt.idabere.adapters;
+package it.unitn.disi.lpsmt.idabere;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 import it.unitn.disi.lpsmt.idabere.R;
 
 public class GpsLoadingActivity extends AppCompatActivity {
 
+    ProgressBar mLoadingIndicator;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gps_loading);
+
+        initViewComps();
+
+    }
+
+    private void initViewComps () {
+
+        mLoadingIndicator = (ProgressBar) findViewById(R.id.progressBar);
+
     }
 }
