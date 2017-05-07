640458e48f5005710c8da6183c19808c72f3b726
xGioe <giovannimaria.riva@studenti.unitn.it>
2017-05-07 15:06:30 +0200

    First commit. Add empty activity

---
+++ iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/adapters/GpsLoadingActivity.java
@@ -0,0 +1,15 @@
+package it.unitn.disi.lpsmt.idabere.adapters;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import it.unitn.disi.lpsmt.idabere.R;
+
+public class GpsLoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gps_loading);
+    }
+}
