753ce10e2167a0a695621a320773be797c92965f
xGioe <giovannimaria.riva@studenti.unitn.it>
2017-05-08 09:36:17 +0200

    Add list items layouts. Change theme colors. Add dialog style to activity

--- iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/AddToppingsActivity.java
+++ iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/AddToppingsActivity.java
@@ -2,12 +2,52 @@ package it.unitn.disi.lpsmt.idabere;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class AddToppingsActivity extends AppCompatActivity {
+
+
+//    TODO Creare un array adapter customizzato di prova per visualizzare la lista delle sizes e dei toppings
+
+    private ListView sizesListView;
+    private ListView toppingsListView;
+
+
+    // Fake data variables
+    private final String UNIT_MEASURE = "cl";
+    private ArrayList<String> SIZES_LIST;
+    private ArrayList<String> TOPPINGS_LIST;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_toppings);
+        initComps();
+        initData();
+
+
+        
+
+    }
+
+    private void initComps () {
+        sizesListView = (ListView) findViewById(R.id.sizes_list_view);
+        toppingsListView = (ListView) findViewById(R.id.toppings_list_view);
+    }
+
+    private void initData () {
+
+        SIZES_LIST = new ArrayList();
+        SIZES_LIST.add("25");
+        SIZES_LIST.add("50");
+        SIZES_LIST.add("75");
+
+        TOPPINGS_LIST = new ArrayList();
+        TOPPINGS_LIST.add("Ghiaccio");
+        TOPPINGS_LIST.add("Limone");
+
     }
 }
---
+++ iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/fragments/AddToppingsDialogFragment.java
@@ -0,0 +1,13 @@
+package it.unitn.disi.lpsmt.idabere.fragments;
+
+import android.app.DialogFragment;
+
+/**
+ * Created by giovanni on 07/05/2017.
+ */
+
+public class AddToppingsDialogFragment extends DialogFragment {
+
+
+
+}
