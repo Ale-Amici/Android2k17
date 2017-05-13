df5f97251e97b0220a811f6e7e14e322c9098997
xGioe <giovannimaria.riva@studenti.unitn.it>
2017-05-13 17:14:21 +0200

    fix review-order link

--- iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/ReviewOrderActivity.java
+++ iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/ReviewOrderActivity.java
@@ -2,11 +2,15 @@ package it.unitn.disi.lpsmt.idabere;
 
 import android.app.SearchManager;
 import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ExpandableListView;
 
 import java.util.ArrayList;
@@ -20,6 +24,10 @@ public class ReviewOrderActivity extends AppCompatActivity {
 
     private ReviewOrderExpandableListAdapter reviewOrderExpandableListAdapter;
     private ExpandableListView reviewExpandableList;
+    private BottomNavigationView bottomNavigationMenu;
+
+    private Context mContext;
+
 
     // Fake Data
     List<String> listDataHeader;
@@ -31,6 +39,9 @@ public class ReviewOrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_review_order);
 
         initViewComps();
+
+        mContext = this;
+
         // preparing fake list data
         prepareListData();
 
@@ -39,12 +50,38 @@ public class ReviewOrderActivity extends AppCompatActivity {
         // setting list adapter
         reviewExpandableList.setAdapter(reviewOrderExpandableListAdapter);
 
+        bottomNavigationMenu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                boolean result = false;
+                int itemId = item.getItemId();
+                Intent intent = new Intent();
+
+                switch (itemId) {
+                    case  R.id.navigation_menu_list :
+                        intent.setClass(mContext,MenuActivity.class);
+                        startActivity(intent);
+                        result = true;
+                        break;
+
+                    case R.id.navigation_delivery_type :
+                        intent.setClass(mContext,DeliveryPlaceActivity.class);
+                        startActivity(intent);
+                        result = true;
+                        break;
+                }
+                return result;
+            }
+        });
+
     }
 
     // Instantiate layout elements
     private void initViewComps () {
         // get the listview
         reviewExpandableList = (ExpandableListView) findViewById(R.id.expandable_review_list);
+        // get the bottom navigation menu
+        bottomNavigationMenu =  (BottomNavigationView) findViewById(R.id.review_bottom_navigation);
     }
 
     @Override
