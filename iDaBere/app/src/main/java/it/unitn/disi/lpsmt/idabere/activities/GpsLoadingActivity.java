5dc6deed1126fecd935d04938fafb70a05374cbc
xGioe <giovannimaria.riva@studenti.unitn.it>
2017-05-18 17:21:51 +0200

    implement gps request for coordinates

--- iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/activities/GpsLoadingActivity.java
+++ iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/activities/GpsLoadingActivity.java
@@ -2,30 +2,56 @@ package it.unitn.disi.lpsmt.idabere.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.content.pm.PermissionInfo;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 
+import java.io.IOException;
+import java.security.Permission;
+import java.security.Permissions;
+import java.util.List;
+import java.util.Locale;
+
+import it.unitn.disi.lpsmt.idabere.Manifest;
 import it.unitn.disi.lpsmt.idabere.R;
 
 public class GpsLoadingActivity extends AppCompatActivity implements
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener, ActivityCompat.OnRequestPermissionsResultCallback {
 
     private ProgressBar mLoadingIndicator;
     private Context mContext;
 
     private GoogleApiClient mGoogleApiClient;
     private Location mLastLocation;
+    LocationRequest mLocationRequest;
+
 
     private double mLatitude;
     private double mLongitude;
@@ -48,24 +74,36 @@ public class GpsLoadingActivity extends AppCompatActivity implements
                     .build();
         }
 
-        new LocationsLoader().execute(fakeData);
+        //new LocationsLoader().execute(fakeData);
+
+
 
     }
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
 
-        try {
-            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                    mGoogleApiClient);
-            if (mLastLocation != null) {
-                mLatitude = mLastLocation.getLatitude();
-                mLongitude = mLastLocation.getLongitude();
-            }
-        } catch (SecurityException ex) {
-            Toast.makeText(this,"Permission Denied", Toast.LENGTH_SHORT);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            Log.d("ERROR","No permission");
+
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1000);
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        
+        if (mLastLocation != null) {
+
+            Log.d("TEST",mLastLocation.getLatitude() + " " + mLastLocation.getLongitude());
+
         }
 
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     @Override
@@ -96,12 +134,14 @@ public class GpsLoadingActivity extends AppCompatActivity implements
 
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+    }
+
     public class LocationsLoader extends AsyncTask<String[],String[],String[]>  {
 
         @Override
         protected String[] doInBackground(String[]... params) {
-
-
 
             SystemClock.sleep(3000);
             return new String[0];
@@ -116,5 +156,7 @@ public class GpsLoadingActivity extends AppCompatActivity implements
         }
 
     }
+
+
 
 }
