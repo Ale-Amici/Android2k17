78ec217dcf043fbaa64bbfe9336732f6f65b8eb4
xGioe <giovannimaria.riva@studenti.unitn.it>
2017-05-20 15:31:59 +0200

    change method name. implement json parser

--- iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/DAOInterfacesImpl/BarsDAOImpl.java
+++ iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/DAOInterfacesImpl/BarsDAOImpl.java
@@ -5,12 +5,15 @@ import android.net.Uri;
 import android.util.JsonReader;
 import android.util.Log;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -30,9 +33,22 @@ public class BarsDAOImpl implements BarsDAO {
     private final String BARS_API_BASE_URL = "http://151.80.152.226/";
     final String BARS_ROUTE = "bars";
 
+    private final String BAR_ID = "id";
+    private final String BAR_NAME = "name";
+    private final String BAR_ADDRESS = "address";
+    private final String BAR_OPENING_HOURS_LIST = "openingHours";
+    private final String BAR_LATITUDE = "latitude";
+    private final String BAR_LONGITUDE = "longitude";
+    private final String BAR_DAY_OF_WEEK = "dayOfWeek";
+    private final String BAR_TIME_OPEN = "timeOpen";
+    private final String BAR_WORKING_TIME = "workingTime";
+    private final String BAR_DISTANCE = "distance";
+
+
     @Override
-    public Bar getBarByAddress(Address address) {
-        Bar result = null;
+    public ArrayList<Bar> getBarsByCoordinates (Address address) {
+        ArrayList<Bar> results = null;
+        JSONArray data = null;
 
         //Expected results
         double barLatitude = address.getLatitude();
@@ -63,17 +79,22 @@ public class BarsDAOImpl implements BarsDAO {
             e.printStackTrace();
         }
         try {
-            InputStream in = urlConnection.getInputStream();
+            InputStream inputStream = urlConnection.getInputStream();
 
-            Scanner scanner = new Scanner(in);
-            scanner.useDelimiter("\\A");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
+            StringBuilder sb = new StringBuilder();
 
-            boolean hasInput = scanner.hasNext();
-            if (hasInput) {
-                Log.d("RESULT",scanner.next());
-            } else {
-                return null;
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
             }
+
+            try {
+                data = new JSONArray(sb.toString());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -81,7 +102,22 @@ public class BarsDAOImpl implements BarsDAO {
         }
 
 
-        return result;
+        try {
+
+            results = new ArrayList<>();
+            for (int i = 0; i <data.length(); i++) {
+                JSONObject item = data.getJSONObject(i);
+                Bar newBar = new Bar(item.getInt(BAR_ID),item.getString(BAR_NAME),null,null, null, null,null);
+                results.add(newBar);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Log.d("BARS", results.toString());
+
+        return results;
     }
 
     @Override
