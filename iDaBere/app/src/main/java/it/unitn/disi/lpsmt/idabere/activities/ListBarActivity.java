471b85d5351148794e38f2a385191edc7100ab96
ale <alessandro.amici@studenti.unitn.it>
2017-05-21 00:26:40 +0200

    modifica della activity che mostra la lista dei bar in modo da far funzionare la ricerca tramite searchbar

--- iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/activities/ListBarActivity.java
+++ iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/activities/ListBarActivity.java
@@ -3,6 +3,7 @@ package it.unitn.disi.lpsmt.idabere.activities;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Address;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
@@ -13,12 +14,18 @@ import android.view.SearchEvent;
 import android.view.View;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
+import it.unitn.disi.lpsmt.idabere.Models.Bar;
 import it.unitn.disi.lpsmt.idabere.R;
+import it.unitn.disi.lpsmt.idabere.adapters.BarsArrayAdapter;
 
 public class ListBarActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     private Context mContext;
     private ListView barsListView;
+    private ArrayList<Bar> barsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,21 +35,35 @@ public class ListBarActivity extends AppCompatActivity implements SearchView.OnQ
         initViewComps();
         mContext = this;
 
+        barsList = new ArrayList<>();
+        /****************** FAKE DATA ***/
+        Bar tmpBar = new Bar();
+        tmpBar.setName("Bel bar");
+        barsList.add(tmpBar);
+        tmpBar = new Bar();
+        tmpBar.setName("Bel bar 2");
+        barsList.add(tmpBar);
+
+        tmpBar = new Bar();
+        tmpBar.setName("Bellissimo bar");
+        barsList.add(tmpBar);
+        /********************************/
+        barsListView = (ListView) this.findViewById(R.id.bars_list_view);
+        barsListView.setAdapter(new BarsArrayAdapter(mContext,R.layout.bar_list_item,barsList));
 
 
-        // Get the intent, verify the action and get the query
-        Intent intent = getIntent();
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            //doMySearch(query);
-        }
+//        // Get the intent, verify the action and get the query
+//        Intent intent = getIntent();
+//        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+//            String query = intent.getStringExtra(SearchManager.QUERY);
+//            //doMySearch(query);
+//        }
     }
 
     /** UI elements methods **/
 
     // Instantiate layout elements
     private void initViewComps () {
-        barsListView = (ListView) findViewById(R.id.bars_list_view);
     }
 
 
@@ -60,37 +81,40 @@ public class ListBarActivity extends AppCompatActivity implements SearchView.OnQ
         // Assumes current activity is the searchable activity
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
 
+        searchView.setSubmitButtonEnabled(false);
+        searchView.setOnQueryTextListener(this);
+
         return super.onCreateOptionsMenu(menu);
     }
 
-    // Handle menu action buttons click
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        boolean result = false;
-        int itemId = item.getItemId();
-
-        switch (itemId) {
-            case R.id.action_search_bar_icon :
-                onSearchRequested();
-                result = true;
-                break;
-            case R.id.action_qr_scanner_icon :
-
-                Intent intent = new Intent();
-                intent.setClass(mContext,QrCodeScannerActivity.class);
-                startActivity(intent);
-
-                result = true;
-                break;
-            case R.id.action_clear_search_bar_icon :
-                result = true;
-                break;
-            default:
-                result = super.onOptionsItemSelected(item);
-        }
-
-        return result;
-    }
+//    // Handle menu action buttons click
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        boolean result = false;
+//        int itemId = item.getItemId();
+//
+//        switch (itemId) {
+//            case R.id.action_search_bar_icon :
+//                onSearchRequested();
+//                result = true;
+//                break;
+//            case R.id.action_qr_scanner_icon :
+//
+//                Intent intent = new Intent();
+//                intent.setClass(mContext,QrCodeScannerActivity.class);
+//                startActivity(intent);
+//
+//                result = true;
+//                break;
+//            case R.id.action_clear_search_bar_icon :
+//                result = true;
+//                break;
+//            default:
+//                result = super.onOptionsItemSelected(item);
+//        }
+//
+//        return result;
+//    }
 
     /** Class methods bar methods **/
 
@@ -113,6 +137,7 @@ public class ListBarActivity extends AppCompatActivity implements SearchView.OnQ
 
     @Override
     public boolean onQueryTextChange(String newText) {
-        return false;
+        ((BarsArrayAdapter)barsListView.getAdapter()).getFilter().filter(newText);
+        return true;
     }
 }
