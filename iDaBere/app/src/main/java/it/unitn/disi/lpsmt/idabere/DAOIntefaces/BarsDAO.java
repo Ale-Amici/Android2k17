5ff378f6d7e4590996003208096473b7d8a65a91
Alessandro1995 <alessandro.amici@studenti.unitn.it>
2017-05-20 17:59:02 +0200

    Revert "Bean bar"

--- iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/DAOIntefaces/BarsDAO.java
+++
@@ -1,8 +0,0 @@
-package it.unitn.disi.lpsmt.idabere.DAOIntefaces;
-
-/**
- * Created by giovanni on 14/05/2017.
- */
-
-public interface BarsDAO {
-}
--- iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/DAOIntefaces/OrdersDAO.java
+++
@@ -1,8 +0,0 @@
-package it.unitn.disi.lpsmt.idabere.DAOIntefaces;
-
-/**
- * Created by giovanni on 15/05/2017.
- */
-
-public interface OrdersDAO {
-}
