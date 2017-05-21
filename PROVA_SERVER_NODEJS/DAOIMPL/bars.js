2c4b98152c51fdd480eecdec3fccd94a00293f73
ale <alessandro.amici@studenti.unitn.it>
2017-05-21 10:24:05 +0200

    prima implementazione del DAO per il prelievo dei dati di un singolo bar dal database dato il bar_id

--- PROVA_SERVER_NODEJS/DAOIMPL/bars.js
+++ PROVA_SERVER_NODEJS/DAOIMPL/bars.js
@@ -2,6 +2,7 @@
 
 var Bar = require('../models/bar.js');
 var OpeningHour = require("../models/openingHour.js")
+var MenuItem = require("../models/menuItem.js")
 var dbHelper = require('../DB/dbhelper.js');
 //var logger    =    require('../UTILS/log.js');
 
@@ -59,43 +60,33 @@ var getBars = function(){
     var bars = [];
     return new Promise(function(resolve, reject){
 
-      pool.queryAsync("SELECT ID,name,description, address, latitude, longitude "
-        + " FROM BAR ORDER BY ID ")
-      .then(function(rows1){
+    pool.queryAsync("SELECT ID,name,description, address, latitude, longitude "
+    + " FROM BAR ORDER BY ID ")
+    .then(function(rows1){
         console.log(rows1);//RIEMPIO L'ARRAY DEI BAR
+
         rows1.forEach(function(row, index){
-          bars.push(new Bar()
-              .setName(row["name"])
-              .setId(row["ID"])
-              .setDescription(row["description"])
-              .setAddress(row["address"])
-              .setLatitude(row["latitude"])
-              .setLongitude(row["longitude"])
-              .setOpeningHours([])
-          );
-        });
-        /**************SECONDA QUERY***************/
-        return pool.queryAsync("SELECT * FROM OPENING_HOUR ORDER BY BAR_ID");
-      }).then(function(rows2){
+            bars.push(getBarFromDbRow(row));
+    });
+
+    /**************SECONDA QUERY***************/
+    return pool.queryAsync("SELECT * FROM OPENING_HOUR ORDER BY BAR_ID");
+    }).then(function(rows2){
         // ALGORITMO PER ASSEGNARE GLI ORARI AI BAR GIUSTI
         barsIndex = 0;
         rows2.forEach(function(row,index){
-          console.log(row)
-          while(bars[barsIndex].id != row["BAR_ID"]){
-            barsIndex ++;
-            console.log("Indice dei bar" + barsIndex);
-            if(barsIndex >= bars.length){
-                console.log("PROBLEMA CON I BOUND DELLA FUNZIONE 1");
-                reject("errore");
-            }
-          }
-          bars[barsIndex].openingHours.push(
-            new OpeningHour()
-              .setDayOfWeek(row["day_of_week"])
-              .setTimeOpen(row["time_open"])
-              .setWorkingTime(row["working_time"])
-          );
+            console.log(row)
 
+            while(bars[barsIndex].id != row["BAR_ID"]){
+                barsIndex ++;
+                console.log("Indice dei bar" + barsIndex);
+                if(barsIndex >= bars.length){
+                    console.log("PROBLEMA CON I BOUND DELLA FUNZIONE 1");
+                    reject("errore");
+                }
+            }
+
+            bars[barsIndex].openingHours.push(getOpeningHourFromDbRow(row));
         });
 
         resolve(bars);
@@ -106,7 +97,74 @@ var getBars = function(){
     });
 };
 
+var getBarFromId = function(barId){
+    var pool = dbHelper.getDBPool();
+    var bar;
+    return new Promise(function(resolve, reject){
+        pool.queryAsync("SELECT * FROM BAR WHERE ID = ? ", barId)
+        .then(function(barRow){
+          console.log(barRow);//creo il BAR
+            bar = getBarFromDbRow(barRow);
+
+            /**************SECONDA QUERY***************/
+            return pool.queryAsync("SELECT * FROM OPENING_HOUR WHERE BAR_ID = ?", barId);
+          })
+          .then(function(openingRows){
+              openingRows.forEach(function(row,index){
+                  bar.openingHours.push(
+                      getOpeningHourFromDbRow(row)
+                  )
+              });
+              /**************TERZA QUERY***************/
+              return pool.queryAsync( " SELECT * "
+                                    + " FROM MENU_ITEM MI JOIN ITEM_CATEGORY IC ON(MI.ITEM_CATEGORY_ID = IC.ID) "
+                                    + " WHERE BAR_ID = ?", barId);
+          })
+          .then(function(menuItemRows){
+              menuItemRows.forEach(function(row,index){
+                  bar.menu.push( getMenuItemFromDbRow(row) );
+              });
+
+              resolve(bar);
+          })
+          .catch(function(err){
+              reject(err);
+          });
+
+
+    });
+}
+
+/*********GET OBJECT FROM DB ROW ********/
+var getBarFromDbRow = function(row){
+    return new Bar()
+        .setName(row["name"])
+        .setId(row["ID"])
+        .setDescription(row["description"])
+        .setAddress(row["address"])
+        .setLatitude(row["latitude"])
+        .setLongitude(row["longitude"])
+        .setOpeningHours([])//li prendo nella seconda query
+        .setMenu([])
+}
+
+var getOpeningHourFromDbRow = function(row){
+    return new OpeningHour()
+        .setDayOfWeek(row["day_of_week"])
+        .setTimeOpen(row["time_open"])
+        .setWorkingTime(row["working_time"])
+}
+
+var getMenuItemFromDbRow = function(row){
+    return new MenuItem()
+        .setId(row["ID"])
+        .setName(row["menu_item_name"])
+        .setDescription(row["description"])
+        .setCategory(row["category_name"])
+}
+
 
 
 module.exports.getBarsFromPosition = getBarsFromPosition;
 module.exports.getBars = getBars;
+module.exports.getBarFromId = getBarFromId;
