2f0f2422a23d7b7f4e6c7ab5ec15009e2b6af86d
xGioe <giovannimaria.riva@studenti.unitn.it>
2017-05-21 12:45:28 +0200

    move into searchbar activity. finish deny permission bug

--- iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/activities/SearchBarActivity.java
+++ iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/activities/SearchBarActivity.java
@@ -1,11 +1,17 @@
 package it.unitn.disi.lpsmt.idabere.activities;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.location.Address;
+import android.location.Location;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
@@ -15,8 +21,21 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.SearchEvent;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 import it.unitn.disi.lpsmt.idabere.DAOIntefaces.FactoryDAO;
 import it.unitn.disi.lpsmt.idabere.DAOInterfacesImpl.FactoryDAOImpl;
@@ -24,14 +43,64 @@ import it.unitn.disi.lpsmt.idabere.Models.Bar;
 import it.unitn.disi.lpsmt.idabere.R;
 import it.unitn.disi.lpsmt.idabere.utils.GpsLocationRetriever;
 
-public class SearchBarActivity extends AppCompatActivity {
+public class SearchBarActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener,
+        ActivityCompat.OnRequestPermissionsResultCallback {
+
+    public boolean coordinatesRetrieveSuccess = false;
+
+    /**
+     * The desired interval for location updates. Inexact. Updates may be more or less frequent.
+     */
+    public static final long UPDATE_INTERVAL_IN_MILLISECONDS = 5000;
+
+    /**
+     * The fastest rate for active location updates. Exact. Updates will never be more frequent
+     * than this value.
+     */
+    public static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
+            UPDATE_INTERVAL_IN_MILLISECONDS / 2;
+
+    /**
+     * Constant used in the location settings dialog.
+     */
+    protected final int REQUEST_CHECK_SETTINGS = 0x1;
+
+    /**
+     * Tracks the status of the location updates request.
+     */
+    protected Boolean mRequestingLocationUpdates;
+
+    /**
+     * Stores parameters for requests to the FusedLocationProviderApi.
+     */
+    protected LocationRequest mLocationRequest;
+
+    /**
+     * Stores the types of location services the client is interested in using. Used for checking
+     * settings to determine if the device has optimal location settings.
+     */
+
+    protected LocationSettingsRequest mLocationSettingsRequest;
+    /**
+     * Represents a geographical location.
+     */
+
+    private GoogleApiClient mGoogleApiClient;
+
+    protected Location mCurrentLocation;
+
+    final String[] mPermissions = new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+    final int mRequestCode = 1000;
+
 
     private final String TAG = "SeachBarActivity";
-
     private Context mContext;
     public static FactoryDAO factoryDAO = new FactoryDAOImpl();
-
     private GpsLocationRetriever gpsLocationRetriever;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,22 +117,69 @@ public class SearchBarActivity extends AppCompatActivity {
             //doMySearch(query);
         }
 
-        gpsLocationRetriever = new GpsLocationRetriever(this, this);
+        initGoogleApiClient();
+        createLocationRequest();
+        buildLocationSettingsRequest();
 
-        new GpsLoader().execute();
+        //gpsLocationRetriever = new GpsLocationRetriever(this, this);
 
 
     }
 
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    protected void onResume() {
+        if (mGoogleApiClient.isConnected()){
+            if (coordinatesRetrieveSuccess){
+                startLocationUpdates();
+            } else {
+                stopLocationUpdates();
+            }
+        }
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        stopLocationUpdates();
+        super.onPause();
+    }
+
     private void updateCoordinates() {
-        new GpsLoader().execute();
+        if (mGoogleApiClient.isConnected()){
+            startLocationUpdates();
+        } else {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    private void initGoogleApiClient() {
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
     }
 
 
     private class GpsLoader extends AsyncTask<Address, Void, ArrayList<Bar>> {
         @Override
         protected void onPreExecute() {
-            gpsLocationRetriever.startConnection();
             super.onPreExecute();
         }
 
@@ -72,16 +188,13 @@ public class SearchBarActivity extends AppCompatActivity {
 
             ArrayList<Bar> bars = null;
 
-            while (gpsLocationRetriever.getmRequestingLocationUpdates()) {
-                //Log.d("WAIT", "Waiting...");
-            }
-
-            Log.d(TAG, "DONE");
-
-            if (gpsLocationRetriever.isCoordinatesRetrieveSuccess()) {
+            if (coordinatesRetrieveSuccess) {
                 bars = new ArrayList<>();
-                Log.d("LOCATION", gpsLocationRetriever.getmAddress().toString());
-                bars = SearchBarActivity.factoryDAO.newBarsDAO().getBarsByCoordinates(gpsLocationRetriever.getmAddress());
+                Log.d("LOCATION", mCurrentLocation.toString());
+                Address address = new Address(Locale.ITALIAN);
+                address.setLongitude(mCurrentLocation.getLongitude());
+                address.setLatitude(mCurrentLocation.getLatitude());
+                bars = SearchBarActivity.factoryDAO.newBarsDAO().getBarsByCoordinates(address);
             }
 
             return bars;
@@ -89,7 +202,6 @@ public class SearchBarActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(ArrayList<Bar> bars) {
-
 
             //Log.d("BARS",bars.toString());
             super.onPostExecute(bars);
@@ -111,20 +223,197 @@ public class SearchBarActivity extends AppCompatActivity {
                     case Activity.RESULT_OK:
                         Log.i(TAG, "User agreed to make required location settings changes.");
                         // Nothing to do. startLocationupdates() gets called in onResume again.
+                        coordinatesRetrieveSuccess = true;
                         break;
                     case Activity.RESULT_CANCELED:
                         Log.i(TAG, "User chose not to make required location settings changes.");
-                        gpsLocationRetriever.stopConnection();
+                        coordinatesRetrieveSuccess = false;
                         break;
                 }
                 break;
         }
     }
 
+    /**
+     * Runs when a GoogleApiClient object successfully connects.
+     */
     @Override
-    protected void onResume() {
-        gpsLocationRetriever.startConnection();
-        super.onResume();
+    public void onConnected(Bundle connectionHint) {
+        // If the initial location was never previously requested, we use
+        // FusedLocationApi.getLastLocation() to get it. If it was previously requested, we store
+        // its value in the Bundle and check for it in onCreate(). We
+        // do not request it again unless the user specifically requests location updates by pressing
+        // the Start Updates button.
+        //
+        // Because we cache the value of the initial location in the Bundle, it means that if the
+        // user launches the activity,
+        // moves to a new location, and then changes the device orientation, the original location
+        // is displayed as the activity is re-created.
+        if (mCurrentLocation == null) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, mPermissions, mRequestCode);
+                return;
+            }
+            mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        }
+        if (mRequestingLocationUpdates) {
+            Log.i(TAG, "in onConnected(), starting location updates");
+            startLocationUpdates();
+        }
+
+    }
+
+    /**
+     * Sets up the location request. Android has two location request settings:
+     * {@code ACCESS_COARSE_LOCATION} and {@code ACCESS_FINE_LOCATION}. These settings control
+     * the accuracy of the current location. This sample uses ACCESS_FINE_LOCATION, as defined in
+     * the AndroidManifest.xml.
+     * <p/>
+     * When the ACCESS_FINE_LOCATION setting is specified, combined with a fast update
+     * interval (5 seconds), the Fused Location Provider API returns location updates that are
+     * accurate to within a few feet.
+     * <p/>
+     * These settings are appropriate for mapping applications that show real-time location
+     * updates.
+     */
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+
+        // Sets the desired interval for active location updates. This interval is
+        // inexact. You may not receive updates at all if no location sources are available, or
+        // you may receive them slower than requested. You may also receive updates faster than
+        // requested if other applications are requesting location at a faster interval.
+        mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        // Sets the fastest rate for active location updates. This interval is exact, and your
+        // application will never receive updates faster than this value.
+        mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    /**
+     * Uses a {@link com.google.android.gms.location.LocationSettingsRequest.Builder} to build
+     * a {@link com.google.android.gms.location.LocationSettingsRequest} that is used for checking
+     * if a device has the needed location settings.
+     */
+    protected void buildLocationSettingsRequest() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        mLocationSettingsRequest = builder.build();
+    }
+
+    /**
+     * Requests location updates from the FusedLocationApi.
+     */
+    protected void startLocationUpdates() {
+        LocationServices.SettingsApi.checkLocationSettings(
+                mGoogleApiClient,
+                mLocationSettingsRequest
+        ).setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult locationSettingsResult) {
+                final Status status = locationSettingsResult.getStatus();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        Log.i(TAG, "All location settings are satisfied.");
+
+                        if (ActivityCompat.checkSelfPermission(SearchBarActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(SearchBarActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                            if (ActivityCompat.shouldShowRequestPermissionRationale(SearchBarActivity.this, mPermissions[0])){
+                                stopLocationUpdates();
+                            }
+                            ActivityCompat.requestPermissions(SearchBarActivity.this, mPermissions,mRequestCode);
+                            return;
+                        }
+                        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, SearchBarActivity.this);
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        if (!coordinatesRetrieveSuccess){
+                            try {
+                                // Show the dialog by calling startResolutionForResult(), and check the
+                                // result in onActivityResult().
+                                status.startResolutionForResult(SearchBarActivity.this, REQUEST_CHECK_SETTINGS);
+                            } catch (IntentSender.SendIntentException e) {
+                                Log.i(TAG, "PendingIntent unable to execute request.");
+                            }
+                        }
+                        Log.i(TAG, "Location settings are not satisfied. Attempting to upgrade " +
+                                "location settings ");
+
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        String errorMessage = "Location settings are inadequate, and cannot be " +
+                                "fixed here. Fix in Settings.";
+                        Log.e(TAG, errorMessage);
+                        Toast.makeText(SearchBarActivity.this, errorMessage, Toast.LENGTH_LONG).show();
+                        mRequestingLocationUpdates = false;
+                }
+
+            }
+        });
+
+    }
+
+    /**
+     * Removes location updates from the FusedLocationApi.
+     */
+    protected void stopLocationUpdates() {
+        // It is a good practice to remove location requests when the activity is in a paused or
+        // stopped state. Doing so helps battery performance and is especially
+        // recommended in applications that request frequent location updates.
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient,
+                this
+        ).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(Status status) {
+                coordinatesRetrieveSuccess = false;
+                mRequestingLocationUpdates = false;
+            }
+        });
+    }
+
+    /**
+     * Callback that fires when the location changes.
+     */
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        coordinatesRetrieveSuccess = true;
+        new GpsLoader().execute();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case mRequestCode :
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted
+                    startLocationUpdates();
+
+                } else {
+                    // permission denied
+                    coordinatesRetrieveSuccess = false;
+                    mRequestingLocationUpdates = false;
+                }
+                break;
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 
     /** UI elements methods **/
@@ -190,10 +479,12 @@ public class SearchBarActivity extends AppCompatActivity {
         return result;
     }
 
+
     public void goToMenuTest (View v) {
         Intent intent = new Intent();
         intent.setClass(mContext,MenuActivity.class);
         startActivity(intent);
     }
+
 
 }
