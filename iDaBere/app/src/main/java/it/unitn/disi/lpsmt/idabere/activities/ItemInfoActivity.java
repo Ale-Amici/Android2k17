e7d4985f67f2b8ff692566449b9a5f3dc2fdc66f
xGioe <giovannimaria.riva@studenti.unitn.it>
2017-05-27 12:05:39 +0200

    add internet check in onStart activity method

--- iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/activities/ItemInfoActivity.java
+++ iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/activities/ItemInfoActivity.java
@@ -12,7 +12,7 @@ public class ItemInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_info);
 
-        // Set back arrow button
+
 
     }
 }
