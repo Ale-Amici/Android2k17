6a54997351580e3931c4d52623959b3adae513f5
xGioe <giovannimaria.riva@studenti.unitn.it>
2017-05-27 12:05:19 +0200

    Create AppStatus java to check for internet connection

---
+++ iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/utils/AppStatus.java
@@ -0,0 +1,42 @@
+package it.unitn.disi.lpsmt.idabere.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+/**
+ * Created by giovanni on 27/05/2017.
+ */
+
+public class AppStatus {
+
+    private static AppStatus instance = new AppStatus();
+    static Context context;
+    ConnectivityManager connectivityManager;
+    NetworkInfo wifiInfo, mobileInfo;
+    boolean connected = false;
+
+    public static AppStatus getInstance(Context ctx) {
+        context = ctx.getApplicationContext();
+        return instance;
+    }
+
+    public boolean isOnline() {
+        try {
+            connectivityManager = (ConnectivityManager) context
+                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            connected = networkInfo != null && networkInfo.isAvailable() &&
+                    networkInfo.isConnected();
+            return connected;
+
+
+        } catch (Exception e) {
+            System.out.println("CheckConnectivity Exception: " + e.getMessage());
+            Log.v("connectivity", e.toString());
+        }
+        return connected;
+    }
+}
