47ad2dd48eb155d6506431d8df8b81eecd707044
xGioe <giovannimaria.riva@studenti.unitn.it>
2017-06-03 17:36:48 +0200

    define users routes

--- PROVA_SERVER_NODEJS/routes/users.js
+++ PROVA_SERVER_NODEJS/routes/users.js
@@ -2,35 +2,40 @@ var express = require('express');
 var router = express.Router();
 var usersCtrl = require('../controllers/users.js');
 
-/* GET employees listing. */
-router.get('/', function (req, res, next) {
-    usersCtrl.index(req, res);
-});
+/* GET users listing. */
+// router.get('/', function (req, res, next) {
+//   usersCtrl.index(req, res);
+// });
 
 
-// /* GET new employee form. */
+/* GET new user form. */
 // router.get('/new', function (req, res, next) {
 //     usersCtrl.new(req, res);
 // });
-//
-// /* POST create employee form. */
-// router.post('/create', function (req, res, next) {
-//     usersCtrl.create(req, res);
-// });
-//
-// /* GET edit employee form. */
+
+/* POST authenticate user. */
+router.post('/create', function (req, res, next) {
+  usersCtrl.create(req, res);
+});
+
+/* POST create user. */
+router.post('/create', function (req, res, next) {
+  usersCtrl.create(req, res);
+});
+
+/* GET edit user. */
 // router.get('/edit/:id', function (req, res, next) {
 //     usersCtrl.edit(req, res);
 // });
-//
-// /* POST update employee. */
-// router.post('/update/:id', function (req, res, next) {
-//     usersCtrl.update(req, res);
-// });
-//
-// /* GET delete employee. */
-// router.get('/destroy/:id', function (req, res, next) {
-//     usersCtrl.destroy(req, res);
-// });
+
+/* POST update user. */
+router.post('/update/:id', function (req, res, next) {
+  usersCtrl.update(req, res);
+});
+
+/* GET delete user. */
+router.get('/destroy/:id', function (req, res, next) {
+  usersCtrl.destroy(req, res);
+});
 
 module.exports = router;
