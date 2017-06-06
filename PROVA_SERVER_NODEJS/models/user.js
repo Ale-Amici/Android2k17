9ac73e9366eb1190f12f09b459281120b43c8092
xGioe <giovannimaria.riva@studenti.unitn.it>
2017-06-06 15:08:21 +0200

    update user.js with missing variables

--- PROVA_SERVER_NODEJS/models/user.js
+++ PROVA_SERVER_NODEJS/models/user.js
@@ -7,7 +7,18 @@ class user {
     this.id =id;
     return this;
   }
-
+  setAge(age){
+    this.age = age;
+    return this;
+  }
+  setEmail(email){
+    this.email = email;
+    return this;
+  }
+  set(password){
+    this.password = password;
+    return this;
+  }
 }
 
 module.exports = user;
