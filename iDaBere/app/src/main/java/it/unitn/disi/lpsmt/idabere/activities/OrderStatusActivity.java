2d0578aa11caca3a46a21c5852cb9bf01602c4bc
xGioe <giovannimaria.riva@studenti.unitn.it>
2017-06-19 09:57:48 +0200

    add asynctask token loader. add check permission. add permisison infos to manifest

--- iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/activities/OrderStatusActivity.java
+++ iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/activities/OrderStatusActivity.java
@@ -1,9 +1,19 @@
 package it.unitn.disi.lpsmt.idabere.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.net.URL;
 
 import it.unitn.disi.lpsmt.idabere.R;
+import me.pushy.sdk.Pushy;
 
 public class OrderStatusActivity extends AppCompatActivity {
 
@@ -11,5 +21,53 @@ public class OrderStatusActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_status);
+
+        Pushy.listen(this);
+        // Check whether the user has granted us the READ/WRITE_EXTERNAL_STORAGE permissions
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // Request both READ_EXTERNAL_STORAGE and WRITE_EXTERNAL_STORAGE so that the
+            // Pushy SDK will be able to persist the device token in the external storage
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+        }
+
+        new RegisterForPushNotificationsAsync().execute();
+
+
     }
+
+    private class RegisterForPushNotificationsAsync extends AsyncTask<Void, Void, Exception> {
+        protected Exception doInBackground(Void... params) {
+            try {
+                // Assign a unique token to this device
+                String deviceToken = Pushy.register(getApplicationContext());
+
+                // Log it for debugging purposes
+                Log.d("MyApp", "Pushy device token: " + deviceToken);
+
+                // Send the token to your backend server via an HTTP GET request
+                new URL("https://{YOUR_API_HOSTNAME}/register/device?token=" + deviceToken).openConnection();
+            }
+            catch (Exception exc) {
+                // Return exc to onPostExecute
+                return exc;
+            }
+
+            // Success
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Exception exc) {
+            // Failed?
+            if (exc != null) {
+                // Show error as toast message
+                Toast.makeText(getApplicationContext(), exc.toString(), Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            // Succeeded, do something to alert the user
+        }
+    }
+
+
 }
