592044ee81cd0146d5a99d68165faea599a366a3
ale <alessandro.amici@studenti.unitn.it>
2017-06-24 21:01:20 +0200

    aggiunta logica ReviewAdapter, non carica i preferiti e i deals nelle categorie e non mostra il bottone delle info

--- iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/adapters/ReviewOrderExpandableListAdapter.java
+++ iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/adapters/ReviewOrderExpandableListAdapter.java
@@ -1,17 +1,20 @@
 package it.unitn.disi.lpsmt.idabere.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.ExpandableListView;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.HashMap;
 import java.util.List;
 
 import it.unitn.disi.lpsmt.idabere.R;
+import it.unitn.disi.lpsmt.idabere.activities.ItemInfoActivity;
 import it.unitn.disi.lpsmt.idabere.models.BarMenu;
 
 /**
@@ -25,5 +28,20 @@ public class ReviewOrderExpandableListAdapter extends MenuCategoryExpandableList
         super(context, originalBarMenu, totalPriceInfo, mExpandableListView);
     }
 
+    @Override
+    protected void setMenuForAdapter(BarMenu barMenu) {
+        setMenuItemCategories(barMenu);
 
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        final View itemView = super.getChildView(groupPosition, childPosition, isLastChild, convertView, parent);
+        //DISABILITO LE INFO
+        final ImageButton itemInfoImageButton = (ImageButton) itemView.findViewById(R.id.item_info_button);
+        itemInfoImageButton.setVisibility(View.INVISIBLE);
+        itemInfoImageButton.setOnClickListener(null);
+
+        return itemView;
+    }
 }
