825704a0f241c299af3af227a2d3acb5bac54f3a
xGioe <giovannimaria.riva@studenti.unitn.it>
2017-06-24 10:46:15 +0200

    first implementation of sendOrder asynctask

--- iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/activities/CheckoutActivity.java
+++ iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/activities/CheckoutActivity.java
@@ -2,6 +2,7 @@ package it.unitn.disi.lpsmt.idabere.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -10,6 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ExpandableListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DecimalFormat;
 
@@ -17,6 +19,7 @@ import it.unitn.disi.lpsmt.idabere.R;
 import it.unitn.disi.lpsmt.idabere.adapters.CheckoutExpandableListAdapter;
 import it.unitn.disi.lpsmt.idabere.models.Order;
 import it.unitn.disi.lpsmt.idabere.session.AppSession;
+import it.unitn.disi.lpsmt.idabere.utils.AppStatus;
 
 
 public class CheckoutActivity extends AppCompatActivity {
@@ -56,8 +59,12 @@ public class CheckoutActivity extends AppCompatActivity {
                         break;
 
                     case R.id.navigation_confirm_payment:
-                        intent.setClass(mContext,OrderStatusActivity.class);
-                        startActivity(intent);
+                        if (isAuthenthicate() && AppStatus.getInstance(mContext).isOnline()){
+                            intent.setClass(mContext,OrderStatusActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(mContext, "Nessuna connessione dati attiva", Toast.LENGTH_SHORT).show();
+                        }
                         result = true;
                         break;
                 }
@@ -89,6 +96,34 @@ public class CheckoutActivity extends AppCompatActivity {
         deliveryType = (TextView) findViewById(R.id.delivery_type_choosen);
         paymentMethod = (TextView) findViewById(R.id.payment_method_choosen);
         getDeliveryTypeDetails = (TextView) findViewById(R.id.delivery_type_value);
+    }
+
+    private boolean isAuthenthicate () {
+        boolean result = false;
+        if (AppSession.getInstance().getmCustomer().getId() != -1) {
+            result = true;
+        } else {
+            // Start Login activity for authentication
+            Intent loginIntent = new Intent();
+            loginIntent.setClass(mContext, LoginActivity.class);
+            startActivity(loginIntent);
+        }
+        return result;
+    }
+
+    private class SendOrderAsyncTask extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected void onPreExecute() {
+            Order currentOrder = AppSession.getInstance().getmCustomer().getOrder();
+            
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            return null;
+        }
     }
 
 }
