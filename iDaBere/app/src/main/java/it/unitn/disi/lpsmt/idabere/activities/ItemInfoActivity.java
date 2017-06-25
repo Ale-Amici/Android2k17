b047b72351e71acec8507024ac78fd483bd127ee
xGioe <giovannimaria.riva@studenti.unitn.it>
2017-06-25 17:35:04 +0200

    finish implementation of add preferred menu button

--- iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/activities/ItemInfoActivity.java
+++ iDaBere/app/src/main/java/it/unitn/disi/lpsmt/idabere/activities/ItemInfoActivity.java
@@ -1,16 +1,22 @@
 package it.unitn.disi.lpsmt.idabere.activities;
 
-import android.media.Rating;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
 import it.unitn.disi.lpsmt.idabere.R;
+import it.unitn.disi.lpsmt.idabere.models.BarMenu;
 import it.unitn.disi.lpsmt.idabere.models.BarMenuItem;
+import it.unitn.disi.lpsmt.idabere.models.Customer;
 import it.unitn.disi.lpsmt.idabere.session.AppSession;
 
 public class ItemInfoActivity extends AppCompatActivity {
@@ -18,6 +24,8 @@ public class ItemInfoActivity extends AppCompatActivity {
     private RatingBar mItemRatingBar;
     private TextView mItemDescription;
     private ImageView mItemImage;
+
+    private MenuItem preferredButton;
 
     private BarMenuItem menuItem;
     private int menuItemId;
@@ -35,7 +43,69 @@ public class ItemInfoActivity extends AppCompatActivity {
         menuItem = AppSession.getInstance().getmBar().getBarMenu().getBarMenuItemById(menuItemId);
         initViewComps();
 
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//            togglePreferredButtonState();
+//        }
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.item_info_menu, menu);
+        preferredButton = menu.findItem(R.id.add_preferred_button);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            togglePreferredButtonState();
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        boolean result = false;
+
+        switch (item.getItemId()){
+            case R.id.add_preferred_button :
+                addtoPreferreds();
+                result = true;
+                break;
+        }
+        return false;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void togglePreferredButtonState() {
+        AppSession currentSession = AppSession.getInstance();
+        BarMenu currentBarMenu = currentSession.getmBar().getBarMenu();
+        Customer currentCustomer = currentSession.getmCustomer();
+        BarMenuItem currentBarMenuItem = currentBarMenu.getBarMenuItemById(menuItemId);
+        Log.d("INDEX", Integer.toString(currentCustomer.getPreferredItems().indexOf(currentBarMenuItem)));
+        if (currentCustomer.getPreferredItems().size() == 0 || currentCustomer.getPreferredItems().indexOf(currentBarMenuItem) == -1 ){
+            preferredButton.setIcon(getDrawable(R.drawable.ic_bookmark_border_black_24dp));
+        } else {
+            preferredButton.setIcon(getDrawable(R.drawable.ic_bookmark_black_24dp));
+        }
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void addtoPreferreds () {
+        AppSession currentSession = AppSession.getInstance();
+        BarMenu currentBarMenu = currentSession.getmBar().getBarMenu();
+        Customer currentCustomer = currentSession.getmCustomer();
+        BarMenuItem currentBarMenuItem = currentBarMenu.getBarMenuItemById(menuItemId);
+        if (currentCustomer.getPreferredItems().size() == 0 || currentCustomer.getPreferredItems().indexOf(currentBarMenuItem) == -1 ){
+            currentCustomer.getPreferredItems().add(currentBarMenuItem);
+            togglePreferredButtonState();
+        } else {
+            currentCustomer.getPreferredItems().remove(currentBarMenuItem);
+            togglePreferredButtonState();
+        }
+        Log.d("PREFERRED ITEMS", currentCustomer.getPreferredItems().toString());
+
+    }
+
 
     private void initViewComps() {
         mItemDescription = (TextView) findViewById(R.id.item_info_content);
